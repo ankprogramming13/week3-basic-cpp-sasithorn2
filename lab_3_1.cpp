811e06f49423d3aeeb1d3a9b819b7e780354cbaf
Unknown <toeyhom64@gmail.com>
2017-11-26 20:08:33 +0700

    ข้อ1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,10 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+   cout << "Ms. : " << "Sasithorn Kanjaroen" << endl;
+  cout << "My nickname is : Toey " << endl;
+  cout << "Sex : Famale" << endl;
+  cout << "Birthday : 06 April 1999" << endl;
+cout << "Hobbies : play computer" << endl;
   return 0;
 }
